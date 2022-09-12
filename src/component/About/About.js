b8dbe058635b0164769fd9ff2b647957ce4935fd
Nur-NobyRahaman <github943@gmail.com>
2022-09-12 21:32:15 +0600

    checkin and about add

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h2>About us</h2>
+        <div className='mt-1 '>
+            <div>
+            <h1 className='m-5'>About Me</h1>
+            </div>
+            <h4>Name:NurNoby Rahaman</h4>
+            <h4>Phone:01710258850</h4>
+            <h4>Home:Tangail</h4>
+            
         </div>
     );
 };
--- src/component/Checkin/Checkin.js
+++ src/component/Checkin/Checkin.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 const Checkin = () => {
     return (
-        <div>
-            <h3>Checkin</h3>
+        <div className='mt-5'>
+            <h1 className='text-primary'>ADMISSION</h1>
+            <div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 g-5 mt-2'>
+               <div className='d-block mx-auto m'>
+               <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">  
+                        <Form.Control type="text" placeholder="Your Name" />   
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">  
+                        <Form.Control type="email" placeholder="Enter email" readOnly/>   
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">  
+                        <Form.Control type="text" placeholder="Phone Number" />   
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        
+                        <Form.Control type="text" placeholder="Exercise Type" readOnly />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Payment?" />
+                    </Form.Group>
+                    <Button className='w-100' variant="primary" type="submit">
+                        Check In
+                    </Button>
+                </Form>
+               </div>
+            </div>
         </div>
     );
 };
